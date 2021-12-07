44ec18b5cd36af92baef93625b74d626b7c01b65
Similiukas <tautvaisassimas@gmail.com>
2021-12-07 11:40:31 +0000

    Some cool notes

--- 2021-Go/src/days/day07.go
+++ 2021-Go/src/days/day07.go
@@ -13,6 +13,11 @@ func someWeirdMath(partOne bool) float64 {
 	// Then the lowest possible fuel amount is the answer
 	// For part two, the x is an arithmetic progression: x = 1 + 2 + ... + x_m = m*(m+1)/2
 	// Hence need to solve for |coord[0]-x|*(|coord[0]-x|+1)/2 + ... + |coord[n]-x|*(|coord[n]-x|+1)/2 = fuel
+
+	// To minimze the sum of absolute values median can be used: https://math.stackexchange.com/q/113270
+	// Lots of people solved this way by just guessing, interesting https://www.reddit.com/r/adventofcode/comments/rars4g/comment/hnk7n2z/?utm_source=share&utm_medium=web2x&context=3
+	// For part two, mean is really close for the optimal solution, however it works only for n^2 and not n(n+1)
+	// But the error is usually really small (<0.3) https://www.reddit.com/r/adventofcode/comments/rar7ty/comment/hnk6gz0/?utm_source=share&utm_medium=web2x&context=3
 	coords := helpers.SliceStringToInt(strings.Split(helpers.GetFirstFileLine("day07"), ","))
 	fuel := math.MaxFloat64
 	for x := 0; x < 1200; x++ {
