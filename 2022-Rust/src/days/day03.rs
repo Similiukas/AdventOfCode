d63691893e7ba25833d13b762db606de0883f1c4
Similiukas <tautvaisassimas@gmail.com>
2022-12-08 21:16:48 +0200

    finish part two at home

--- 2022-Rust/src/days/day03.rs
+++ 2022-Rust/src/days/day03.rs
@@ -1,14 +1,3 @@
-use std::iter::Iterator;
-use std::fs::File;
-use std::io::{self, BufRead};
-// use std::iter::FromIterator;
-
-// fn sort_string_by_char(input: &str) -> String {
-//     let mut arr: Vec<char> = input.chars().collect();
-//     arr.sort_by(|a, b| a.cmp(b));
-//     return String::from_iter(arr);
-// }
-
 fn find_item_priorities() -> u32 {
     let result: u32 = include_str!("../../input/day03/input.txt")
         .split('\n')
@@ -29,28 +18,23 @@ fn find_item_priorities() -> u32 {
 
 fn find_badges() -> u32 {
     let mut result: u32 = 0; let mut line_index: u32 = 0;
-    let mut line1 = "".to_string(); let mut line2 = "".to_string();
+    let mut line1 = ""; let mut line2 = "";
 
-    let file = File::open("input/day03/input.txt").unwrap();
-    let data = io::BufReader::new(file).lines();
+    let lines = include_str!("../../input/day03/input.txt").lines();
 
-    for line_wrap in data {
-        let line = line_wrap.unwrap_or("".to_string());
+    for line in lines {
         if line_index == 0 {
-            line1 = line.clone();
+            line1 = line;
             line_index += 1;
         } else if line_index == 1 {
-            line2 = line.clone();
+            line2 = line;
             line_index += 1;
         } else if line_index == 2 {
             line_index = 0;
-            for item in line1.chars() {
-                if line2.contains(item) {
-                    if line.chars().any(|x| x == item) {
-                        result += if item as u32 > 96 { item as u32 - 96 } else { item as u32 - 38 };
-                        println!("Found in all lines {} {} {} {} {}", item, if item as u32 > 96 { item as u32 - 96 } else { item as u32 - 38 }, line1, line2, line);
-                    }
-                    // break;
+            for item in line.chars() {
+                if line1.contains(item) && line2.chars().any(|x| x == item) {
+                    result += if item as u32 > 96 { item as u32 - 96 } else { item as u32 - 38 };
+                    break;
                 }
             }
         }
@@ -61,6 +45,6 @@ fn find_badges() -> u32 {
 
 pub fn solution() {
     println!("Another day, another work solver");
-    // println!("Result: {}", find_item_priorities());
+    println!("Result: {}", find_item_priorities());
     println!("Second one: {}", find_badges());
 }
