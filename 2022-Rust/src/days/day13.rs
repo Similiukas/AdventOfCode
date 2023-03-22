39ccf00009ea35adfa2b0f8f4883f7bb3b4fdf37
Similiukas <tautvaisassimas@gmail.com>
2023-03-22 14:40:49 +0000

    nice and simple modifcation for part two

--- 2022-Rust/src/days/day13.rs
+++ 2022-Rust/src/days/day13.rs
@@ -1,4 +1,4 @@
-use std::vec;
+use std::{vec, cmp::Ordering};
 
 // Checking if string type is array (for example: "[2,3]")
 fn type_is_arr(item: &String) -> bool {
@@ -58,47 +58,67 @@ fn parser(_input: String) -> Vec<String> {
 }
 
 // Returns whether the items are in the right order
-// 1 -> correct order
-// 0 -> undetermined
-// -1 -> wrong order
-fn compare(left: Vec<String>, right: Vec<String>) -> i8 {
+// Less -> correct order
+// Equal -> undetermined
+// Greater -> wrong order
+fn compare(left: Vec<String>, right: Vec<String>) -> Ordering {
     for (i, item_a) in left.iter().enumerate() {
         // Right ran out of items before left, wrong order
-        if i + 1 > right.len() { return -1 }
+        if i + 1 > right.len() { return Ordering::Greater }
 
         let item_b = &right[i];
         // Both are integers
         if !type_is_arr(item_a) && !type_is_arr(item_b){
-            if item_a.parse::<i32>().unwrap() < item_b.parse::<i32>().unwrap() { return 1 }
-            if item_a.parse::<i32>().unwrap() > item_b.parse::<i32>().unwrap() { return -1 }
+            if item_a.parse::<i32>().unwrap() < item_b.parse::<i32>().unwrap() { return Ordering::Less }
+            if item_a.parse::<i32>().unwrap() > item_b.parse::<i32>().unwrap() { return Ordering::Greater }
         }
          // Both are lists
         else if type_is_arr(item_a) && type_is_arr(item_b) {
             let result = compare(parser(item_a.clone()), parser(item_b.clone()));
-            if result != 0 { return result }
+            if result.is_ne() { return result }
         }
         else {
             let result = compare(parser(convert_to_vec(item_a)), parser(convert_to_vec(item_b)));
-            if result != 0 { return result }
+            if result.is_ne() { return result }
         }
     }
     // Left ran out of items, if right is longer then it's the correct order, otherwise undetermined
-    if left.len() < right.len() { return 1 }
-    0
+    if left.len() < right.len() { return Ordering::Less }
+    Ordering::Equal
 }
 
-
+// Part one
 fn get_indices() -> usize {
     let mut result = 0;
     let data: Vec<&str> = include_str!("../../input/day13/input.txt").split("\n\n").collect();
     for (i, pair) in data.iter().enumerate() {
         let l: Vec<&str> = pair.split('\n').collect();
-        if compare(vec![l[0].to_string()], vec![l[1].to_string()]) > 0 { result += i + 1 }
+        if compare(vec![l[0].to_string()], vec![l[1].to_string()]).is_lt() { result += i + 1 }
     }
     result
+}
+
+// Part two
+fn get_sorted_indices() -> usize {
+    let divider_1 = "[[2]]"; let divider_2 = "[[6]]";
+    let mut first_divider = 0;
+
+    let mut data: Vec<String> = include_str!("../../input/day13/input.txt").split("\n\n").fold(vec![],|acc, pair| {
+        [acc, pair.lines().map(|item| { item.to_string()}).collect() ].concat()
+    });
+    data.push(divider_1.to_string());
+    data.push(divider_2.to_string());
+
+    data.sort_unstable_by(|a, b| compare(vec![a.to_string()], vec![b.to_string()]));
+    for (i, pair) in data.iter().enumerate() {
+        if pair == divider_1 { first_divider = i + 1 }
+        else if pair == divider_2 { return first_divider * (i + 1) }
+    }
+    0
 }
 
 pub fn solution() {
     println!("Started to go way slower but I guess still going?");
     println!("The right order indices sum {}", get_indices());
+    println!("The sorted indices decoder key: {}", get_sorted_indices());
 }
